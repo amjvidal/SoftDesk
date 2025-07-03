d3e708a2b4f89da5dec297c39ff77eb96d4501a0
amjvidal <antoniomariojesus123@gmail.com>
2025-07-03 15:56:13 -0300

    corrigindo registro aluno e outro
    corrigindo o form dos html e nas rotas das paginas de registro de aluno e outros

--- routes/registro_outro.py
+++ routes/registro_outro.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 registroOutro_routes = Blueprint('registroOutro',__name__)
 
@@ -9,5 +9,20 @@ registroOutro_routes = Blueprint('registroOutro',__name__)
 @registroOutro_routes.route('/registroOutro', methods=['GET','POST'])
 def registroOutro():
     """ Retorna a página de registro de outros """
+    inputs = [
+    {'id': 'nome', 'type': 'text', 'placeholder': 'Nome Completo *', 'name': 'nome'},
+    {'id': 'email', 'type': 'email', 'placeholder': 'Email (discente) *', 'name': 'email'},
+    {'id': 'cpf', 'type': 'text', 'placeholder': '000.000.000-00', 'name': 'cpf'},
+    {'id': 'ocupacao', 'type': 'text', 'placeholder': 'Ocupação *', 'name': 'ocupacao'},
+    {'id': 'senha', 'type': 'password', 'placeholder': 'Senha *', 'name': 'senha'},
+    {'id': 'confirmaSenha', 'type': 'password', 'placeholder': 'Confirme sua senha *', 'name': 'confirmaSenha'},
+    {'id': 'bio', 'type': 'textarea', 'placeholder': 'Fale sobre você', 'name': 'bio'},
+]
     
-    return render_template('registro_outros.html')
+    if request.method == 'POST':
+        dados = {campo['name']: request.form.get(campo['name']) for campo in inputs}
+        print("Dados recebidos no formulário:")
+        for chave, valor in dados.items():
+            print(f"{chave}: {valor}")
+    
+    return render_template('registro_outros.html', inputs=inputs)
