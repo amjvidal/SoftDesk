d62cf50f940ef7f2110c7cf916a330553df64049
amjvidal <antoniomariojesus123@gmail.com>
2025-07-03 21:18:40 -0300

    fix no form de login

--- routes/login.py
+++ routes/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 login_routes = Blueprint('login',__name__)
 
@@ -9,5 +9,15 @@ login_routes = Blueprint('login',__name__)
 @login_routes.route('/', methods=['GET','POST'])
 def login():
     """ Retorna a página de login """
+    inputs = [
+    {'id': 'email', 'type': 'email', 'placeholder': 'Email', 'name': 'email'},
+    {'id': 'senha', 'type': 'password', 'placeholder': 'Senha', 'name': 'senha'},
+    ]
     
-    return render_template('login.html')
+    if request.method == 'POST':
+        dados = {campo['name']: request.form.get(campo['name']) for campo in inputs}
+        print("Dados recebidos no formulário:")
+        for chave, valor in dados.items():
+            print(f"{chave}: {valor}")
+    
+    return render_template('login.html', inputs=inputs)
