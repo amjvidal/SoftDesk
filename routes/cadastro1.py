991b3ba21dc1b0e333cb89ff1a9c8e74e9a7d8f2
amjvidal <antoniomariojesus123@gmail.com>
2025-07-07 15:42:17 -0300

    back da area de cadastro

--- routes/cadastro1.py
+++ routes/cadastro1.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect
 
 cadastro_routes = Blueprint('cadastro',__name__)
 
 """ Rotas de home
     - /cadastro- Get - Retorna a página de cadastro
-    - /cadastro- Post - Cadastra o usuário
+    - /cadastro- Post - Leva o usuario para as paginas de registro
 """
 @cadastro_routes.route('/cadastro', methods=['GET','POST'])
 def cadastro():
     """ Retorna a página de cadastro """
+    acao = request.form.get('acao')
     
+    if acao == 'aluno':
+        return redirect(url_for('registroAluno.registroAluno'))
+    elif acao == 'outros':
+        return redirect(url_for('registroOutro.registroOutro'))
+    elif acao == 'voltar':
+        return redirect(url_for('login.login'))
+        
     return render_template('cadastro1.html')
