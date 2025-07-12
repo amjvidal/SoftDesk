e89dc8714868d9fedaff054d7525b008fdaa3300
Thiago Leal <lealthiago435@gmail.com>
2025-07-11 23:49:10 -0300

    Adicionando telas de menu de usuário: home, minhasDemandas, criarDemanda e verDetalhes

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -11,12 +11,19 @@ from routes.cadastro1 import cadastro_routes
 from routes.recuperar import recuperar_routes
 from routes.registro_aluno import registroAluno_routes
 from routes.registro_outro import registroOutro_routes
+from routes.home import home_routes
+from routes.home_demandas import home_demandas_routes
+from routes.home_nova_demanda import home_nova_demanda_routes
+from routes.home_detalhes_demanda import home_detalhes_demanda_routes
 
 app.register_blueprint(login_routes)
 app.register_blueprint(cadastro_routes)
 app.register_blueprint(recuperar_routes)
 app.register_blueprint(registroAluno_routes)
 app.register_blueprint(registroOutro_routes)
-
+app.register_blueprint(home_routes)
+app.register_blueprint(home_demandas_routes)
+app.register_blueprint(home_nova_demanda_routes)
+app.register_blueprint(home_detalhes_demanda_routes)
 
 app.run(debug=True)
